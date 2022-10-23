efe97914f5de9a0b94570723e9253a350387eaf1
Gavin Gamboa <gavin@strangeloop-studios.com>
2022-10-22 21:47:15 -0700

    style updates

--- site/templates/layouts/default.php
+++ site/templates/layouts/default.php
@@ -1,5 +1,5 @@
 <?= $rockfrontend->renderIf("sections/main.latte", "template=default-page") ?>
-<?= $rockfrontend->renderIf("sections/activities.latte", "template=activities") ?>
+<?= $rockfrontend->renderIf("sections/recent.latte", "template=recent") ?>
 <!-- <?= $rockfrontend->renderIf("sections/index.latte", "parent=projects") ?> -->
 <?= $rockfrontend->renderIf("sections/music-videos.latte", "template=music-videos") ?>
 <?= $rockfrontend->renderIf("sections/about.latte", "name=about") ?>
