8055e8c79083f81049bbf94f0af0cba62e30e93f
Gavin Gamboa <gavin@strangeloop-studios.com>
2022-10-27 12:54:48 -0700

    projects index with image hov

--- site/templates/layouts/default.php
+++ site/templates/layouts/default.php
@@ -1,6 +1,6 @@
 <?= $rockfrontend->renderIf("sections/main.latte", "template=default-page") ?>
 <?= $rockfrontend->renderIf("sections/projects.latte", "name=projects") ?>
 <?= $rockfrontend->renderIf("sections/recent.latte", "template=recent") ?>
-<!-- <?= $rockfrontend->renderIf("sections/index.latte", "parent=projects") ?> -->
+<?= $rockfrontend->renderIf("sections/index.latte", "parent=projects") ?>
 <?= $rockfrontend->renderIf("sections/music-videos.latte", "template=music-videos") ?>
 <?= $rockfrontend->renderIf("sections/about.latte", "name=about") ?>
