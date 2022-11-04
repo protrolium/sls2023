34406b2e03fe64900e89f68962e50d5668cef568
Gavin Gamboa <gavin@strangeloop-studios.com>
2022-11-04 00:03:22 -0700

    improve dark mode logo assets switching

--- site/templates/scripts/main.js
+++ site/templates/scripts/main.js
@@ -1,3 +1,6 @@
+// const mySettings = ProcessWire.config.mySettings;
+// console.log(mySettings);
+
 // theme switcher
 const html = document.querySelector('html');
 html.dataset.theme = `theme-light`;
@@ -6,17 +9,42 @@ function switchTheme(theme) {
     html.dataset.theme = `theme-${theme}`;
 }
 
+// logos switcher
 function toggleTheme() {
+
+    const logosList = document.querySelectorAll("#logo-header, #logo-footer");
+    const logosArray = [...logosList];
+    const logosLightList = document.querySelectorAll("#logo-light-header, #logo-light-footer");
+    const logosLightArray = [...logosLightList];
+
     if (html.dataset.theme === 'theme-light') {
         switchTheme('dark');
         document.getElementById("dark-mode-btn").innerHTML = '🌕';
+        
+        logosArray.forEach(element => {
+            element.classList.add("uk-hidden");
+        });
+        
+        logosLightArray.forEach(element => {
+            element.classList.remove("uk-hidden");
+        });
+        
     } else {
         switchTheme('light');
         document.getElementById("dark-mode-btn").innerHTML = '🌑';
+        
+        logosArray.forEach(element => {
+            element.classList.remove("uk-hidden");
+        });
+        
+        logosLightArray.forEach(element => {
+            element.classList.add("uk-hidden");
+        });
     }
 }
 
 // listen to os preference
+const isOsDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 const matchMediaPrefDark = window.matchMedia('(prefers-color-scheme: dark)');
 
 function startListeningToOSTheme() {
