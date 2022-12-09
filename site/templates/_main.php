3c1eeaad03f0f8b3e9b01a8aa09b4ce080182e7e
Gavin Gamboa <gavin@strangeloop-studios.com>
2022-12-09 15:22:44 -0800

    studio.latte and header adjustments

--- site/templates/_main.php
+++ site/templates/_main.php
@@ -33,6 +33,7 @@ $rockfrontend
 		<style>html{visibility: hidden;opacity:0;}</style>
 
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		
 		<!-- make sure we get styling on mobile by setting meta viewport -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title><?php echo $page->title; ?></title>
