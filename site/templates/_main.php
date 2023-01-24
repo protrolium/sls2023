4ac4b34792e537889c261bbb13a60ca48830ff7f
Gavin Gamboa <gavin@strangeloop-studios.com>
2023-01-24 14:44:16 -0800

    processwire attriburtion

--- site/templates/_main.php
+++ site/templates/_main.php
@@ -51,6 +51,7 @@ $rockfrontend
 		<title><?php echo $page->title; ?></title>
 		<meta name="description" content="Strangeloop Studios is a visual content production company, focused on working with creatives to explore new media and audience experiences. Our team is composed of animators, designers, producers, writers, musicians, and developers.">
 		<meta name="keywords" content="Visuals, Concert Visuals, Animation, 3D Animator, Notch, C4D, Motion Graphics, Design, Creative Studio, Music, Music Visuals, Musicians, Tour, Projection, Video Projection, Projection Mapping, 3D, Virtual Reality, VR, VJ, Visualist, Video Design, Programmer, Fractals, Hyper, Production Company, Los Angeles, Flying Lotus, The Weeknd, Kendrick Lamar, Zeds Dead, Erykah Badu, Lil Nas X">
+		<meta name="generator" content="ProcessWire">
 
 		<!-- fonts -->
 		<link rel="preconnect" href="https://fonts.googleapis.com">
