20cc3d5762c82cd09224a40fc2ba22fd7e5607d5
Gavin Gamboa <gavin@strangeloop-studios.com>
2023-08-24 18:01:41 -0700

    removing ignored files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'de4e2f051821a82e77e135ea8e65d9c4c4a85daf',
+        'name' => 'processwire/processwire',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'processwire/processwire' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'de4e2f051821a82e77e135ea8e65d9c4c4a85daf',
+            'dev_requirement' => false,
+        ),
+        'vlucas/valitron' => array(
+            'pretty_version' => 'v1.4.11',
+            'version' => '1.4.11.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../vlucas/valitron',
+            'aliases' => array(),
+            'reference' => 'fadce39f5f235755bb9794b2573af2d5bfcba85f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
