4d27689ea97f53c9539a2b40b6588ab11f9ec727
Gavin Gamboa <gavin@strangeloop-studios.com>
2023-08-28 17:54:02 -0700

    metatags readjust

--- site/templates/_main.php
+++ site/templates/_main.php
@@ -46,17 +46,21 @@ $rockfrontend
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		
 		<!-- make sure we get styling on mobile by setting meta viewport -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Strangeloop Studios · <?php echo $page->title; ?></title>
-		<meta name="description" content="Strangeloop Studios is a visual content production company, focused on working with creatives to explore new media and audience experiences. Our team is composed of animators, designers, producers, writers, musicians, and developers.">
+		<meta name="description" content="Strangeloop Studios is a visual content production company, focused on working with creatives to explore new media and audience experiences. Our team is composed of animators, designers, producers, writers, musicians, and developers."> -->
+		
+		<!-- metatags -->
+		<?php $metadata = $modules->get('MarkupMetadata');?>
+		<?php echo $metadata->render();?> 
 		<meta name="keywords" content="Visuals, Concert Visuals, Animation, 3D Animator, Notch, C4D, Motion Graphics, Design, Creative Studio, Music, Music Visuals, Musicians, Tour, Projection, Video Projection, Projection Mapping, 3D, Virtual Reality, VR, VJ, Visualist, Video Design, Programmer, Fractals, Hyper, Production Company, Los Angeles, Flying Lotus, The Weeknd, Kendrick Lamar, Zeds Dead, Erykah Badu, Lil Nas X">
 		<meta name="generator" content="ProcessWire">
-
+		
 		<!-- fonts -->
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link href="https://fonts.googleapis.com/css2?&family=Oswald:wght@200;300;400;500;600;700&family=Silkscreen&display=swap" rel="stylesheet"> 
-
+		
 		<!-- favicons -->
 		<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $config->urls->assets?>favicon/apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="<?php echo $config->urls->assets?>favicon/favicon-32x32.png">
@@ -65,10 +69,7 @@ $rockfrontend
 		<link rel="mask-icon" href="<?php echo $config->urls->assets?>favicon/safari-pinned-tab.svg" color="#5bbad5">
 		<meta name="msapplication-TileColor" content="#da532c">
 		<meta name="theme-color" content="#ffffff">
-
-		<!-- metatags -->
-		<?php $metadata = $modules->get('MarkupMetadata');?>
-		<?php echo $metadata->render();?>
+		
 
 	</head>
 	<body id="html-body">
