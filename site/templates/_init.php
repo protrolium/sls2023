a66a1894099cc934a641c5fb9e591826a7c5d576
TRAPPIST <in@gav.cloud>
2025-03-09 19:57:20 -0700

    local dev env init

--- site/templates/_init.php
+++ site/templates/_init.php
@@ -1,10 +1,37 @@
-<?php // namespace ProcessWire;
+<?php namespace ProcessWire;
 
 // Optional initialization file, called before rendering any template file.
 // This is defined by $config->appendTemplateFile in /site/config.php.
 // Use this to define shared variables, functions, classes, includes, etc. 
 
 // include_once('/assets/inc/mailchimp.php');
+
+if ($config->rockdevtools) {
+    $devtools = rockdevtools();
+    
+    // compile all less files to CSS
+    $devtools->assets()
+        ->less() 
+        ->add('/site/templates/uikit/src/less/uikit.theme.less')
+        ->add('/site/templates/sections/**.less', 3)
+        ->add('/site/templates/styles/custom.less')
+        ->save('/site/templates/src/.styles.css');
+  
+    // merge and minify css files
+    $devtools->assets()
+        ->css()
+        ->add('/site/templates/src/.styles.css')
+        ->save('/site/templates/dst/styles.min.css');
+  
+    // merge and minify JS files
+    $devtools->assets()
+        ->js()
+        ->add('/site/templates/uikit/dist/js/uikit.min.js')
+        ->add('/site/templates/uikit/dist/js/uikit-icons.min.js')
+        ->add('/site/templates/scripts/main.js')
+        ->save('/site/templates/dst/scripts.min.js');
+  }
+
 
 function isMobileDevice() {
     // Get the user agent string
